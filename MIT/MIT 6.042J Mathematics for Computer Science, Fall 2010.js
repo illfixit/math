ae3ba228fe2dfac25e0ca88614be27943ce8c169
eslessons <eslessons@tutanota.com>
2020-11-02 14:06:03 +0300

    Finish Lecture 3 : Strong Induction

---
+++ MIT/MIT 6.042J Mathematics for Computer Science, Fall 2010.js
@@ -0,0 +1,46 @@
+/* MIT 6.042J 
+Mathematics for Computer Science 
+Fall 2010
+
+Instructor: Tom Leighton
+View the complete course: http://ocw.mit.edu/6-042JF10
+Youtube playlist: https://www.youtube.com/playlist?list=PLWX710qNZo_sT8y9Nt8vyC_fXOEo-ebTS
+
+Lecture 1: Introduction and Proofs
+Start
+Finish
+
+Lecture 2: Induction
+Start
+Finish
+
+Lecture 3: Strong Induction
+Start
+Finish
+
+Lecture 4:
+Lecture 5:
+Lecture 6:
+Lecture 7:
+Lecture 8:
+Lecture 9:
+Lecture 10:
+
+Lecture 11:
+Lecture 12:
+Lecture 13:
+Lecture 14:
+Lecture 15:
+Lecture 16:
+Lecture 17:
+Lecture 18:
+Lecture 19:
+Lecture 20:
+
+Lecture 21:
+Lecture 22:
+Lecture 23:
+Lecture 24:
+Lecture 25:
+
+*/
