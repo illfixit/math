2cebe8f6ee3a1987f23f4e60f56e2bcc0b5de8d5
ES Lessons <eslessons@tutanota.com>
2021-01-05 17:00:51 +0300

    Finish Hour 2

---
+++ Calculus 1 - Full Course.js
@@ -0,0 +1,159 @@
+/* youtube.com > freeCodeCamp.org
+
+Calculus 1 - Full College Course
+
+https://www.youtube.com/watch?v=HfACrKJ_Y2w
+
+(0:00:00) [Corequisite] Rational Expressions
+Start
+Finish
+
+(0:09:40) [Corequisite] Difference Quotient
+Start
+Finish
+
+(0:18:20) Graphs and Limits
+Start
+Finish
+
+(0:25:51) When Limits Fail to Exist
+Start
+Finish
+
+(0:31:28) Limit Laws
+Start
+Finish
+
+(0:37:07) The Squeeze Theorem
+Start
+Finish
+
+(0:42:55) Limits using Algebraic Tricks
+Start
+Finish
+
+(0:56:04) When the Limit of the Denominator is 0
+Start
+Finish
+
+(1:08:40) [Corequisite] Lines: Graphs and Equations
+Start
+Finish
+
+(1:17:09) [Corequisite] Rational Functions and Graphs
+Start
+Finish
+
+(1:30:35) Limits at Infinity and Graphs
+Start
+Finish
+
+(1:37:31) Limits at Infinity and Algebraic Tricks
+Start
+Finish
+
+(1:45:34) Continuity at a Point
+Start
+Finish
+
+(1:53:21) Continuity on Intervals
+Start
+Finish
+
+(1:59:43) Intermediate Value Theorem
+Start
+Finish
+
+(2:03:37) [Corequisite] Right Angle Trigonometry
+Start
+Finish
+
+(2:11:13) [Corequisite] Sine and Cosine of Special Angles
+Start
+Finish
+
+(2:19:16) [Corequisite] Unit Circle Definition of Sine and Cosine
+Start
+Finish
+
+(2:24:46) [Corequisite] Properties of Trig Functions
+Start
+Finish
+
+(2:35:25) [Corequisite] Graphs of Sine and Cosine
+Start
+Finish
+
+(2:41:57) [Corequisite] Graphs of Sinusoidal Functions
+Start
+Finish
+
+(2:52:10) [Corequisite] Graphs of Tan, Sec, Cot, Csc
+Start
+Finish
+
+(3:01:03) [Corequisite] Solving Basic Trig Equations
+(3:08:14) Derivatives and Tangent Lines
+(3:22:55) Computing Derivatives from the Definition
+(3:34:02) Interpreting Derivatives
+(3:42:33) Derivatives as Functions and Graphs of Derivatives
+(3:56:25) Proof that Differentiable Functions are Continuous
+(4:01:09) Power Rule and Other Rules for Derivatives
+(4:07:42) [Corequisite] Trig Identities
+(4:15:14) [Corequisite] Pythagorean Identities
+(4:20:35) [Corequisite] Angle Sum and Difference Formulas
+(4:28:31) [Corequisite] Double Angle Formulas
+(4:36:01) Higher Order Derivatives and Notation
+(4:39:22) Derivative of e^x
+(4:46:52) Proof of the Power Rule and Other Derivative Rules
+(4:56:31) Product Rule and Quotient Rule
+(5:02:09) Proof of Product Rule and Quotient Rule
+(5:10:40) Special Trigonometric Limits
+(5:17:31) [Corequisite] Composition of Functions
+(5:29:54) [Corequisite] Solving Rational Equations
+(5:40:02) Derivatives of Trig Functions
+(5:46:23) Proof of Trigonometric Limits and Derivatives
+(5:54:38) Rectilinear Motion
+(6:11:41) Marginal Cost
+(6:16:51) [Corequisite] Logarithms: Introduction
+(6:25:32) [Corequisite] Log Functions and Their Graphs
+(6:36:17) [Corequisite] Combining Logs and Exponents
+(6:40:55) [Corequisite] Log Rules
+(6:49:27) The Chain Rule
+(6:58:44) More Chain Rule Examples and Justification
+(7:07:43) Justification of the Chain Rule
+(7:10:00) Implicit Differentiation
+(7:20:28) Derivatives of Exponential Functions
+(7:25:38) Derivatives of Log Functions
+(7:29:38) Logarithmic Differentiation
+(7:37:08) [Corequisite] Inverse Functions
+(7:51:22) Inverse Trig Functions
+(8:00:56) Derivatives of Inverse Trigonometric Functions
+(8:12:11) Related Rates - Distances
+(8:17:55) Related Rates - Volume and Flow
+(8:22:21) Related Rates - Angle and Rotation
+(8:28:20) [Corequisite] Solving Right Triangles
+(8:34:54) Maximums and Minimums
+(8:46:18) First Derivative Test and Second Derivative Test
+(8:51:37) Extreme Value Examples
+(9:01:33) Mean Value Theorem
+(9:09:09) Proof of Mean Value Theorem
+(0:14:59) [Corequisite] Solving Right Triangles
+(9:25:20) Derivatives and the Shape of the Graph
+(9:33:31) Linear Approximation
+(9:48:28) The Differential
+(9:59:11) L'Hospital's Rule
+(10:06:27) L'Hospital's Rule on Other Indeterminate Forms
+(10:16:13) Newtons Method
+(10:27:45) Antiderivatives
+(10:33:24) Finding Antiderivatives Using Initial Conditions
+(10:41:59) Any Two Antiderivatives Differ by a Constant
+(10:45:19) Summation Notation
+(10:49:12) Approximating Area
+(11:04:22) The Fundamental Theorem of Calculus, Part 1
+(11:15:02) The Fundamental Theorem of Calculus, Part 2
+(11:22:17) Proof of the Fundamental Theorem of Calculus
+(11:29:18) The Substitution Method
+(11:38:07) Why U-Substitution Works
+(11:40:23) Average Value of a Function
+(11:47:57) Proof of the Mean Value Theorem for Integrals
