af2bea4e40265f0acef891ed6915d5f3b048e041
ES Lessons <eslessons@tutanota.com>
2021-01-27 18:27:06 +0300

    Start MIT 18.01 - Single Variable Calculus -> Single Variable Calculus

---
+++ Calculus for Machine learning.js
@@ -0,0 +1,32 @@
+/* youtube.com > Academic Lesson
+Calculus for Machine learning
+
+https://www.youtube.com/watch?v=8stueNPVl-I
+
+(0:05)         A Preview of Calculus
+(37:25)       The Limit of a Function. 
+(1:14:30)    The Limit Laws
+(1:51:00)    Continuity
+(2:16:52)    The Precise Definition of a Limit
+(2:45:52)    Defining the Derivative
+(3:10:16)    The Derivative as a Function
+(3:34:28)    Differentiation Rules
+(4:04:05)    Derivatives as Rates of Change
+(4:39:40)    Derivatives of Trigonometric Functions
+(4:55:30)    The Chain Rule
+(5:15:08)    Derivatives of Inverse Functions
+(5:40:18)    Implicit Differentiation
+(6:06:28)    Derivatives of Exponential and Logarithmic Functions
+(6:31:32)    Partial Derivatives
+(6:53:10)    Related Rates
+(7:19:48)    Linear Approximations and Differentials
+(7:42:56)    Maxima and Minima
+(8:01:59)    The Mean Value Theorem
+(8:21:21)    Derivatives and the Shape of a Graph
+(8:45:59)    Limits at Infinity and Asymptotes
+(9:11:35)    Applied Optimization Problems
+(9:42:36)    L'Hopital's Rule
+(10:14:01)  Newton's Method
+(10:35:24)   Antiderivatives
+
+*/
---
+++ MIT/MIT 18.01 Single Variable Calculus, Fall 2007.js
@@ -0,0 +1,50 @@
+/* MIT 18.01 
+Single Variable Calculus
+Fall 2007
+Instructor: Prof. David Jerison
+
+View the complete course: http://ocw.mit.edu/18-01F06
+Youtube playlist: 
+https://www.youtube.com/playlist?list=PL590CCC2BC5AF3BC1
+
+Lecture 01: Derivatives, slope, velocity, rate of change
+Start
+
+
+Lecture 02: Limits, continuity; Trigonometric limits
+Lecture 03: Derivatives of products, quotients, sine, cosine
+Lecture 04: Chain rule. Higher derivatives
+Lecture 05: Implicit differentiation, inverses
+Lecture 06: Exponential and log; Logarithmic differentiation; hyperbolic functions
+Lecture 07: Hyperbolic functions (cont.) and exam 1 review
+Lecture 09: Linear and quadratic approximations
+
+Lecture 10: Approximations (cont.); curve sketching
+Lecture 11: Max-min problems
+Lecture 12: Related rates
+Lecture 13: Newton's method and other applications
+Lecture 14: Mean value theorem; Inequalities
+Lecture 15: Differentials, antiderivatives
+Lecture 16: Differential equations, separation of variables
+Lecture 18: Definite integrals
+Lecture 19: First fundamental theorem of calculus
+
+Lecture 20: Second fundamental theorem
+Lecture 21: Applications to logarithms and geometry
+Lecture 22: Volumes by disks and shells
+Lecture 23: Work, average value, probability
+Lecture 24: Numerical integration
+Lecture 25: Exam 3 review
+Lecture 27: Trigonometric integrals and substitution
+Lecture 28: Integration by inverse substitution; completing the square
+Lecture 29: Partial fractions
+
+Lecture 30: Integration by parts, reduction formulae
+Lecture 31: Parametric equations, arclength, surface area
+Lecture 32: Polar coordinates; area in polar coordinates
+Lecture 33: Exam 4 review
+Lecture 35: Indeterminate forms - L'Hospital's rule
+Lecture 36: Improper integrals
+Lecture 37: Infinite series and convergence tests
+Lecture 38: Taylor's series
+Lecture 39: Final review
